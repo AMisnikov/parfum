20a15fd05506ddd2d2ba36423cd4e3c4ef33603b
Alexander Misnikov <www.start@inbox.ru>
2019-06-26 17:32:37 +1000

    uploading project

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//DOM Elements
+
+const burgerBtn = document.querySelector('.btn-burger');
+const navigation = document.querySelector('.main-nav');
+const toTopBtn = document.querySelector('.btn-to-top');
+
+
+//Burger Manipulation
+
+burgerBtn.addEventListener( 'click', () => {
+    burgerBtn.classList.toggle('btn-burger--active');
+    navigation.classList.toggle('main-nav--active');
+});
+
+//Scroll to top
+
+toTopBtn.addEventListener('click', up);
+
+
+
+let t;
+function up() {
+  const top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+  if(top > 0) {
+    window.scrollBy(0,-70);
+    t = setTimeout('up()',20);
+  } else clearTimeout(t);
+  return false;
+}
+
+
+
